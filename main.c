15fb1e154665064d585f682aec640d7b4412a388
trerolle <trerolle@student.42lausanne.ch>
2022-10-04 19:42:55 +0200

    add mlx

--- main.c
+++ main.c
@@ -6,29 +6,16 @@
 /*   By: trerolle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 17:51:26 by trerolle          #+#    #+#             */
-/*   Updated: 2022/10/02 15:24:27 by trerolle         ###   ########.fr       */
+/*   Updated: 2022/10/03 20:06:56 by trerolle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/fractol.h"
 
-/*
-void	choose_fractal_set(t_env *env, int argc, char **argv)
-{
-	if (env->fractal_type == 1)
-	{
-		create_mandelbrot_set(env);
-	}
-	if (env->fractal_type == 2)
-	{
-		create_julia_set(env, argv, argc);
-	}
-}*/	
-
 int	main(int argc, char **argv)
 {
 	t_env	*env;
-	(void)argv;
+
 	if (argc < 2)
 		return (0);
 	env = parse_args(argv, argc);
